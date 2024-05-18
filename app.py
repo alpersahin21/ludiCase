5312ce38868fcb589b1064fbea87530da4aee17f
Alper Şahin <alpersahin21@ku.edu.tr>
2024-05-18 16:25:14 +0300

    Case study done.

--- app.py
+++ app.py
@@ -15,20 +15,28 @@ with open('data/simulations.json') as f:
 users_df = pd.DataFrame(users_data['users'])
 simulations_df = pd.DataFrame(simulations_data['simulations'])
 
-# Calculate total users per company
+# calculate users per company
 company_user_counts = users_df.merge(simulations_df, on='simulation_id') \
                               .groupby('company_name') \
                               .size() \
                               .reset_index(name='total_users')
 
-# Calculate total users per day
+# Rename columns
+company_user_counts.columns = ['Şirket İsmi', 'Kullanıcı Sayısı']
+
+# calculate total user development by day
 users_df['signup_datetime'] = pd.to_datetime(users_df['signup_datetime'], unit='d', origin='1899-12-30')
 daily_user_counts = users_df.set_index('signup_datetime').resample('D').size().cumsum().reset_index()
 daily_user_counts.columns = ['dates', 'counts']
 
+# Convert dates to string format
+daily_user_counts['dates'] = daily_user_counts['dates'].dt.strftime('%Y-%m-%d')
+
 @app.route('/')
 def index():
-    return render_template('index.html', tables=[company_user_counts.to_html(classes='table table-striped', header="true")], daily_user_counts=daily_user_counts.to_dict(orient='list'))
+    return render_template('index.html',
+                           tables=[company_user_counts.to_html(classes='table table-striped', header="true", index = False)],
+                           daily_user_counts=daily_user_counts.to_dict(orient='list'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
